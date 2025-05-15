3992b1ef7b40dbf85d4585a879f1db3ce8bae163
EVANS KIPKORIR <EVANOCRUSE@GMAIL.COM>
2025-05-15 23:06:54 +0300

    wrldlist

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    def match(self,word_list):
+        sorted_word = sorted(self.word)
+        matches = []
+
+        for word in word_list:
+            if sorted_word == sorted(word.lower()) and word.lower() != self.word:
+                matches.append(word)
+        return matches
+        
